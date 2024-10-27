3c72a00ee6c70a5f9d15efa3daade3430a7d5a17
Altynbek2021 <shatmanalie0694@gmail.com>
2024-10-27 21:44:10 +0600

    finished login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,12 +6,85 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TextButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        child: Text("Go back"),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Image(
+                image: AssetImage("images/logo.png"),
+                height: 200,
+                width: 200,
+              ),
+            ),
+            InputForm(data: "Enter your email"),
+            SizedBox(
+              height: 30,
+            ),
+            InputForm(data: "Enter your password"),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.lightBlueAccent,
+              ),
+              child: TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginPage()));
+                  },
+                  child: const Text(
+                    "Log in",
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+          ],
+        ),
       ),
+      // body: TextButton(
+      //   onPressed: () {
+      //     Navigator.pop(context);
+      //   },
+      //   child: Text("Go back"),
+      // ),
+    );
+  }
+}
+
+class InputForm extends StatelessWidget {
+  const InputForm({
+    super.key,
+    required this.data,
+  });
+  final String data;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide:
+                const BorderSide(color: Colors.lightBlueAccent, width: 0.0),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide:
+                const BorderSide(color: Colors.lightBlueAccent, width: 0.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide:
+                const BorderSide(color: Colors.lightBlueAccent, width: 0.0),
+          ),
+          labelText: data),
     );
   }
 }
